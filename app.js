64b3ed6c523b345d613741db7c5839f0b1caa6ae
whelan957 <sitdtest@gmail.com>
2017-07-11 16:21:02 +0800

    Update version information.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/static', express.static('public'));
 app.get('/versionInfo', function (req, res) {
-  res.status(200).send('20170710 Support newspaper rendering.');
+  res.status(200).send('20170711 Support PDF generation.');
 });
 //Check rest key
 app.use((req, res, next) => {
